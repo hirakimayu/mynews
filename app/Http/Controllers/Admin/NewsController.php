50c70e07a5deffee7bdd2f36128b0e248e4bd0c3
EC2 Default User <ec2-user@ip-172-31-32-77.ap-northeast-1.compute.internal>
2020-01-08 12:33:16 +0000

    mynews画面

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,9 +7,8 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //
-    public function add()
-    {
-        return view('admin.news.create');
-    }
+ public function add()
+ {
+   return view('admin.news.create');
+ }
 }
